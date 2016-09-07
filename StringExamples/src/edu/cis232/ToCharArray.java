5be9fd41381a0ea79d1f46e7d541b4424f67ad4c
SK207_Inst <SK207_Inst@K207_Inst.carrollcc.edu>
2016-09-07 19:58:49 -0400

    toCharArray example

--- StringExamples/src/edu/cis232/ToCharArray.java
+++ StringExamples/src/edu/cis232/ToCharArray.java
@@ -3,8 +3,24 @@ package edu.cis232;
 public class ToCharArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String str = "Some example text 22";
+		
+		//The original way to loop through the characters in a String
+		for(int i = 0; i < str.length(); i++){
+			char c = str.charAt(i);
+			if(Character.isLetter(c)){
+				System.out.print(c);
+			}
+		}
+		System.out.println();
+		
+		//The enhanced way to loop through the characters in a String
+		for(char c : str.toCharArray()){
+			if(Character.isLetter(c)){
+				System.out.print(c);
+			}
+		}
+		System.out.println();
 	}
 
 }
