8128360c573f9a60a716c99fdced85420a35593a
SK207_Inst <SK207_Inst@K207_Inst.carrollcc.edu>
2016-09-07 19:38:22 -0400

    Lab Solution

--- StringExamples/src/edu/cis232/LabIndexOf.java
+++ StringExamples/src/edu/cis232/LabIndexOf.java
@@ -13,5 +13,25 @@ public class LabIndexOf {
 		System.out.println("Give me some input");
 		String input = keyboard.nextLine();
 		
+		//+3 points, show first C
+		int cIndex = input.indexOf('C');
+		if(cIndex >= 0){
+			System.out.println("C first appears at index " + cIndex);
+		} else {
+			System.out.println("C does not appear in the input");
+		}
+		
+		//+2 points, show every C
+		int cIndex2 = input.indexOf('C');
+		while(cIndex2 >= 0){
+			System.out.println("C at index " + cIndex2);
+			cIndex2 = input.indexOf('C', cIndex2 + 1);
+		}
+		
+		//Case-insensitive search
+		int cIndex3 = input.toUpperCase().indexOf('C');
+		if(cIndex3 >= 0){
+			System.out.println("C or c appears at index " + cIndex3);
+		}
 	}
 }
