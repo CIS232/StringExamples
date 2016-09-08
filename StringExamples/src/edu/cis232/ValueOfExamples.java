59e5fd9b8be0e8ef42d3be7335007bb4f7eb6bfb
SK207_Inst <SK207_Inst@K207_Inst.carrollcc.edu>
2016-09-07 20:06:03 -0400

    valueOf Examples

---
+++ StringExamples/src/edu/cis232/ValueOfExamples.java
@@ -0,0 +1,21 @@
+package edu.cis232;
+
+public class ValueOfExamples {
+	public static void main(String[] args){
+		//Static method 'valueOf' will converts primitive types to a String
+		int tensPlace = 5;
+		
+		int onesPlace = 7;
+		
+		//combine them together to show 57
+		String number = String.valueOf(tensPlace) + String.valueOf(onesPlace);
+		System.out.println(number);
+		
+		
+		float pi = 3.14159f;
+		String piStr = String.valueOf(pi);
+		System.out.println(piStr);
+		//For more control, use String.format
+		System.out.println(String.format("%.2f", pi));
+	}
+}
